5fa8d32a69d6139de1c2b419a1a5038a6ce49f04
Jesper Hellberg <extern.jesper.hellberg@digg.se>
2026-08-05 11:56:46 +0200

    fix: tolerate unknown keys in server response types
    Annotate server-produced types with @JsonIgnoreUnknownKeys so additive
    backend changes do not break clients already installed on devices.
    
    Signed-off-by: Jesper Hellberg <extern.jesper.hellberg@digg.se>

--- access-mechanism/src/main/java/se/digg/wallet/access_mechanism/model/KeyInfo.kt
+++ access-mechanism/src/main/java/se/digg/wallet/access_mechanism/model/KeyInfo.kt
@@ -5,13 +5,17 @@
 package se.digg.wallet.access_mechanism.model
 
 import com.nimbusds.jose.jwk.JWK
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
 import se.digg.wallet.access_mechanism.utils.InstantIso8601Serializer
 import se.digg.wallet.access_mechanism.utils.JwkSerializer
 import java.time.Instant
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
+@JsonIgnoreUnknownKeys
 data class KeyInfo(
     @SerialName("created_at") @Serializable(with = InstantIso8601Serializer::class) val createdAt: Instant,
     @SerialName("public_key") @Serializable(with = JwkSerializer::class) val publicKey: JWK
